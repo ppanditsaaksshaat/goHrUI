8bbe0ac24b3de600b47328e3276a07d122024eba
Deepak Jain <deeepjain1290@gmail.com>
2017-04-17 15:30:08 +0530

    added pageService service

---
+++ src/app/common/rudra.common.js
@@ -0,0 +1,15 @@
+/**
+ * @author deepak.jain
+ * created on 14/04/2017
+ */
+(function () {
+  'use strict';
+
+  angular.module('rudra.common')
+      .service('pageService', pageService);
+
+
+function pageService(){
+
+}
+})
