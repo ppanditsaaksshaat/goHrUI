b31d963795eb1aa5440ad80a3eb4bd47c7927616
Nitesh Mishra <nitesh.mishra@itsl.in>
2017-04-18 15:22:12 +0530

    applicationmodule

---
+++ src/app/pages/leave/applications/detail/applicationsdetail.controller.js
@@ -0,0 +1,18 @@
+/**
+ * @author a.demeshko
+ * created on 28.12.2015
+ */
+(function () {
+  'use strict';
+
+  angular.module('BlurAdmin.pages.leave.masters')
+    .controller('LeaveApplicationsDetailController', LeaveApplicationsDetailController);
+
+  /** @ngInject */
+  function LeaveApplicationsDetailController($stateParams, mailMessages) {
+    var vm = this;
+    vm.mail = mailMessages.getMessageById($stateParams.id);
+    vm.label = $stateParams.label;
+  }
+
+})();
