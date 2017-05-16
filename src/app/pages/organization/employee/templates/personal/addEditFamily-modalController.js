8572efe9b686af5ca5abe480bac0eb6acfd8669d
Pradeep Pandit <pradeep.pandit@itsl.in>
2017-05-16 14:28:32 +0530

    pp:emp

---
+++ src/app/pages/organization/employee/templates/personal/addEditFamily-modalController.js
@@ -0,0 +1,106 @@
+/**
+ * @author v.lugovsky
+ * created on 16.12.2015
+ */
+(function () {
+  'use strict';
+
+  angular.module('BlurAdmin.pages.organization.employee')
+    .controller('addEmployeeFamilyDetails', addEmployeeFamilyDetails);
+
+  /** @ngInject */
+  /** @ngInject */
+  function addEmployeeFamilyDetails($scope, $stateParams, mailMessages,
+    addModal, pageService, editableOptions, editableThemes, DJWebStore, $timeout, param) {
+    console.log(param.FdId)   
+    var rndValu = Math.round((Math.random() * 10) * 10);
+    var rndValu2 = Math.round((Math.random() * rndValu) * rndValu);
+    var vm = this;
+    vm.empPKId = $stateParams.empId;
+    vm.tableId = 56;
+    vm.pageId = 52;
+    var family = {};
+    function _loadController() {
+      $timeout(function () {      
+        pageService.getPagData(vm.pageId).then(
+          _getPageDataSuccessResult, _getPageDataErrorResult);
+      });
+    }
+    function _getPageDataSuccessResult(result) {
+      console.log(result)
+      $scope.relationships = result.pageinfo.selects.FdRelationshipId;
+      if(param.FdId!=0)
+      {
+        pageService.findEntity(56, parseInt(param.FdId), undefined).then(_familyEntitySuccessResult, _familyEntityErrorResult);
+      }
+    }
+    function _getPageDataErrorResult(error) {
+    }
+    function _familyEntitySuccessResult(result) {   
+       $scope.family = result;
+       vm.oldEntity=result;
+       if(param.Action=='view') {
+         $scope.name=true;
+         $scope.relationship=true;
+         $scope.dateofbirth=true;
+         $scope.dependent=true;
+       }
+       else{
+          $scope.name=false;
+         $scope.relationship=false;
+         $scope.dateofbirth=false;
+         $scope.dependent=false;
+       }
+     }
+     function _familyEntityErrorResult(error) {
+
+     }
+
+    function _setupSaving(dataObject,action) {
+      var data = {
+        oldEntity: vm.oldEntity!=null?vm.oldEntity: dataObject,
+        newEntity: dataObject,
+        pageCode: vm.pageId,
+        activity: action
+      }
+      return data;
+    }
+
+    $scope.addFamily = function () {
+      var family = {
+        FdEmpId: vm.empPKId,
+        FdName: $scope.family.FdName,
+        FdRelationshipId: $scope.family.FdRelationshipId,
+        FdDateOfBirth: $scope.family.FdDateOfBirth,
+        FdDependents: $scope.family.FdDependents,
+      }
+      var savingObj = _setupSaving(family,'create');
+      _familyCreatEdit(savingObj);     
+    }
+    function _familyCreatEdit(savingObj)
+    {
+     pageService.editPageData(vm.pageId, JSON.stringify(savingObj)).then(_createEditSuccessResult, _createEditErrorResult)
+    }
+
+    function _createEditSuccessResult(result) {
+      $scope.showMsg('success', 'Employee family member saved successfully');
+    }
+    function _createEditErrorResult(error) {
+    }
+    $scope.editFamily=function(){
+      var family = {  
+        FdId:param.FdId, 
+        FdEmpId: vm.empPKId,
+        FdName: $scope.family.FdName,
+        FdRelationshipId: $scope.family.FdRelationshipId,
+        FdDateOfBirth: $scope.family.FdDateOfBirth,
+        FdDependents: $scope.family.FdDependents,
+      }
+      alert(JSON.stringify(family))
+      var savingObj = _setupSaving(family,'edit');
+      _familyCreatEdit(savingObj);
+    }
+
+    _loadController();
+  }
+})();
