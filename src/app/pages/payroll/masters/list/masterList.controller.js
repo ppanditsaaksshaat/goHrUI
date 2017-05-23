3eb9e4735d5f9819691c562b7aa692469c57d51d
Pradeep Pandit <pradeep.pandit@itsl.in>
2017-05-23 18:11:05 +0530

    emp:pp

--- src/app/pages/payroll/masters/list/masterList.controller.js
+++ src/app/pages/payroll/masters/list/masterList.controller.js
@@ -6,81 +6,46 @@
   'use strict';
 
   angular.module('BlurAdmin.pages.payroll.masters')
-    .controller('PayMastersListController', PayMastersListController);
+    .controller('payMastersListController', payMastersListController);
 
   /** @ngInject */
-  function PayMastersListController($scope, $stateParams,
-    pageService, editableOptions, editableThemes, DJWebStore) {
-
-
-    var rndValu = Math.round((Math.random() * 10) * 10);
-    var rndValu2 = Math.round((Math.random() * rndValu) * rndValu);
+  function payMastersListController($scope, $state, $stateParams,
+    pageService, editableOptions, editableThemes, DJWebStore, dialogModal) {
 
     var vm = this;
-    vm.pageId = $stateParams.pageId;
-    vm.table = { rows: [] }
-    vm.page = {};
-    $scope.isLoading = true;
-    $scope.isLoaded = false;
-    vm.templateUrlPath = '';
-    vm.tempName = $stateParams.name;
-    vm.templateUrlPath = "app/pages/payroll/masters/templates/"
-      + vm.tempName + "/" + vm.tempName + "-list.html?" + rndValu2 + "=" + rndValu;
-    vm.listTemplateUrlPath = 'app/pages/payroll/masters/list/table-list.html';
+    var pageId = $stateParams.pageId;
+    var tempName = $stateParams.name;
+    var currentState = $state.current;
 
-    vm.refreshData = function () {
-      $scope.rows = [];
-      _getTableData();
+
+    $scope.page = $scope.createPage();
+    $scope.page.pageId = pageId;
+    $scope.page.boxOptions = {
+      selfLoading: true,
+      showRefresh: true,
+      showFilter: true,
+      showAdd: true,
+      showRowMenu: true,
+      showCustomView: true,
+      showUpload: false,
+      showDialog: false,
+      enableRefreshAfterUpdate: true,
+      gridHeight: 450,
+      linkColumns: [],
+      getPageData: null,
+      refreshData: null,
+      addRecord: null,
+      editRecord: null,
+      updateRecord: null,
+      viewRecord: null,
+      deleteRecord: null,
+      uploadRecord: null
     }
 
     function _loadController() {
-      pageService.getPagData(vm.pageId).then(_successGetPage, _errorGetPage)
+      //to be implemented
     }
-    function _successGetPage(result) {
-      console.log(result)
-      vm.page = result;
-      // DJWebStore.SetValue('Page_' + vm.pageId, result)
-      $scope.setPage(vm.page)
-      _getTableData();
-    }
-    function _errorGetPage(err) {
 
-    }
-    function _getTableData() {
-      $scope.isLoaded = false
-      $scope.isLoading = true
-      var data = {
-        searchList: [],
-        orderByList: []
-      }
-      var tableData = pageService.getTableData(
-        vm.page.pageinfo.tableid,
-        vm.page.pageinfo.pageid,
-        '', '',
-        false, data);
-
-      tableData.then(_getTableSuccessResult, _getTableErrorResult)
-    }
-    function _getTableErrorResult(err) {
-      $scope.isLoaded = true
-      $scope.isLoading = false
-    }
-    function _getTableSuccessResult(result) {
-      $scope.isLoaded = true
-      $scope.isLoading = false
-      console.log(result)
-      if (result == 'NoDataFound') {
-        // uivm.showMsg('warning', 'No Record Found.');
-      } else if (result.Errors !== undefined) {
-        // uivm.showMsg('error', result.Message);
-        // _startMsgTimer();
-      }
-      else {
-        vm.table.rows = result;
-        $scope.rows = result;
-        console.log($scope.rows.length)
-      }
-    }
     _loadController();
 
   }
--- src/app/pages/payroll/masters/pay.masters.module.js
+++ src/app/pages/payroll/masters/pay.masters.module.js
@@ -16,7 +16,7 @@
         .state('payroll.masters', {
           url: '/masters',
           abstract: true,
-          templateUrl: 'app/pages/payroll/masters/pay.masters.html',
+          templateUrl: 'app/pages/payroll/masters/pay.master.html?v=1',
           controller: "PayMastersController",
           controllerAs: "tabCtrl",
           title: 'Master',
@@ -24,33 +24,33 @@
             order: 0,
           },
         })
-        .state('payroll.masters.list', {
-          url: '/:name/:pageId',
-          templateUrl: 'app/pages/payroll/masters/list/mastersList.html',
-          title: 'payroll Masters',
-          controller: "PayMastersListController",
-          controllerAs: "listCtrl"
-        })
-        //.state('payroll.masters.detail', {
+         .state('payroll.masters.list', {
+           url: '/:name/:pageId',
+         templateUrl: 'app/pages/payroll/masters/list/mastersList.html',
+         title: 'payroll Masters',
+         controller: "PayMastersController",
+         controllerAs: "listCtrl"
+         });
+        // .state('payroll.masters.detail', {
         //   url: '/:pageId/:id',
         //   templateUrl: 'app/pages/payroll/masters/detail/mastersDetail.html',
         //   title: 'payroll Masters',
-        //   controller: "OrgMastersDetailController",
+        //   controller: "attMastersDetailController",
         //   controllerAs: "detailCtrl"
         // }).state('payroll.masters.add', {
         //   url: '/:name/:action/:pageId/',
         //   templateUrl: 'app/pages/payroll/masters/add/add.html',
         //   title: 'payroll Masters',
-        //   controller: "OrgMastersAddController",
+        //   controller: "attMastersAddController1",
         //   controllerAs: "addCtrl"
         // }).state('payroll.masters.edit', {
         //   url: '/:name/:action/:pageId/:pkId/',
         //   templateUrl: 'app/pages/payroll/masters/add/add.html',
         //   title: 'payroll Masters',
-        //   controller: "OrgMastersAddController",
+        //   controller: "attMastersAddController1",
         //   controllerAs: "addCtrl"
         // });
-    // $urlRouterProvider.when('/payroll/masters','/payroll/masters/location/34');
+    $urlRouterProvider.when('/payroll/masters','/payroll/masters/location/34');
   }
 
 })();
