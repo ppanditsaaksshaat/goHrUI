3840b4fa71732619ac63cd3277653f5ba2233a4c
Nitesh Mishra <nitesh.mishra@itsl.in>
2017-06-09 12:44:44 +0530

    leave statement NM

---
+++ src/app/pages/leave/transaction/leavestatement/leave.leavestatement.module.js
@@ -0,0 +1,31 @@
+
+/**
+ * @author santosh.kushwaha
+ * created on 20.05.2017
+ */
+(function () {
+  'use strict';
+
+  angular.module('BlurAdmin.pages.leave.transaction.leavestatement', [])
+    .config(routeConfig);
+
+  /** @ngInject */
+  function routeConfig($stateProvider, $urlRouterProvider) {
+
+    $stateProvider
+      .state('leave.transaction.leavestatement', {
+        url: '/leavestatement',
+      //  abstract: true,
+        templateUrl: 'app/pages/leave/transaction/leavestatement/leave.leavestatement.html?v=1',
+        controller: "LeaveStatementController",
+        controllerAs: "attCtrl",
+        title: 'Leave Statement',
+        sidebarMeta: {
+          order: 0,
+          parent: 'leave.transaction',
+          pageTitle: 'Leave Statement'
+        },
+      })
+  }
+
+})();
