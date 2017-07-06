d8a602ed3b4f352574d397b7cc5ef75f1031ff02
Deepak Jain <deepjain1290@gmail.com>
2017-07-06 10:19:36 +0530

    m:dj

--- src/app/pages/organization/employees/edit/employees.tab.controller.js
+++ src/app/pages/organization/employees/edit/employees.tab.controller.js
@@ -99,18 +99,12 @@
                 vm.templateUrlPath = "app/pages/organization/employees/templates/grid-view.html?" + rndValu2 + "=" + rndValu;
                 console.log(vm.empPKId)
                 // $scope.page = _getLocalPageObject(vm.pageId, 'WEEmpId', vm.empPKId)
-<<<<<<< HEAD
 
-                if (vm.pageId != 360 && vm.pageId != 36)
-                    $scope.page = _getLocalPageObject(vm.pageId)
-                console.log($scope.page);
-=======
-                if (vm.pageId != 360 && vm.pageId != 'entitlement') {
+               if (vm.pageId != 360 && vm.pageId != 'entitlement') {
                     vm.templateUrlPath = "app/pages/organization/employees/templates/grid-view.html?" + rndValu2 + "=" + rndValu;
                     $scope.page = _getLocalPageObject(vm.pageId)
                     console.log($scope.page);
                 }
->>>>>>> 1ac1a777cc46ac5bf91f6bee65e595229a6663f6
             }
         }
 
