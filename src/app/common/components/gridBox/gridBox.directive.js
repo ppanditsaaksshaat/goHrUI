b8c5b5ce296ffdfc049104ccca654b46cbeea627
Deepak Jain <deepjain1290@gmail.com>
2017-07-21 12:05:02 +0530

    payroll:dj

--- src/app/common/components/gridBox/gridBox.directive.js
+++ src/app/common/components/gridBox/gridBox.directive.js
@@ -53,7 +53,8 @@
                     pageResult: null,
                     dataResult: null,
                     saveResult: null,
-                    afterCellEdit: null//external cell edit event
+                    afterCellEdit: null,//external cell edit event
+                    onRegisterApi: null
                 }
 
                 //customButtons, selectedRowButtons: text, icon, onClick, type:btn-detault
@@ -292,6 +293,9 @@
                 function _onRegisterApi(gridApi) {
                     ////console.log('register grid api')
                     $scope.page.gridApi = gridApi;
+                    if ($scope.page.boxOptions.onRegisterApi) {
+                        $scope.page.boxOptions.onRegisterApi(gridApi)
+                    }
 
                     gridApi.selection.on.rowSelectionChanged($scope, function (row) {
                         $scope.page.selectedRows = gridApi.selection.getSelectedRows();
@@ -455,16 +459,25 @@
                 }
                 function _getPageSuccessResult(result) {
                     console.log(result)
-                    $scope.page = angular.extend({}, $scope.page, result);
-                    $scope.page.pageIsLoaded = true;
-                    $scope.page.pageIsLoading = false;
-                    if ($scope.page.boxOptions.pageResult) {
-                        if ($scope.page.boxOptions.pageResult != null) {
-                            $scope.page.boxOptions.pageResult(result);
+                    var isErrored = false;
+                    if (result.error_message) {
+                        $scope.isErrorOccured = true;
+                        if (result.error_message.Message) {
+                            $scope.errorText = result.error_message.Message
                         }
                     }
-                    if ($scope.page.boxOptions.showDataOnLoad)
-                        _refreshData();
+                    else {
+                        $scope.page = angular.extend({}, $scope.page, result);
+                        $scope.page.pageIsLoaded = true;
+                        $scope.page.pageIsLoading = false;
+                        if ($scope.page.boxOptions.pageResult) {
+                            if ($scope.page.boxOptions.pageResult != null) {
+                                $scope.page.boxOptions.pageResult(result);
+                            }
+                        }
+                        if ($scope.page.boxOptions.showDataOnLoad)
+                            _refreshData();
+                    }
                 }
                 function _getPageErrorResult(err) {
                     $scope.page.pageIsLoaded = true;
--- src/app/common/services/webStore.js
+++ src/app/common/services/webStore.js
@@ -80,17 +80,19 @@ angular.module('BlurAdmin.common').factory('DJWebStore', ['localStorageService',
     }
     var _getServiceBase = function () {
 
-        //var serviceBase = 'http://localhost:51877/';
+        var serviceBase = _getValue('serviceBase');
+
+        //uncomment for your choice
+
+        serviceBase = 'http://localhost:51877/';
+        //serviceBase = 'http://rudraitsl.com/api/';// _getValue('serviceBase');
+        //serviceBase = 'http://web300.com/api/';// _getValue('serviceBase');
+        //serviceBase ='http://web200.com/api/';// _getValue('serviceBase');
+        // serviceBase = 'http://web400.hrms/api/';
+        // serviceBase = 'http://localhost/api/';
 
 
 
-        var serviceBase = 'http://web400.hrms/api/';// _getValue('serviceBase');
-        // var serviceBase = 'http://rudraitsl.com/api/';// _getValue('serviceBase');
-
-        // var serviceBase = 'http://web300.com/api/';// _getValue('serviceBase');
-
-
-        //    var serviceBase ='http://web200.com/api/';// _getValue('serviceBase');
         if (serviceBase == null) {
             var host = $location.host();
             var absUrl = $location.absUrl().replace('#/', '');
@@ -132,7 +134,7 @@ angular.module('BlurAdmin.common').factory('DJWebStore', ['localStorageService',
             //token expired 
             if (urlPath.indexOf('auth.html') < 0) {
                 _logout();
-            }   
+            }
             return;
         }
 
@@ -140,7 +142,7 @@ angular.module('BlurAdmin.common').factory('DJWebStore', ['localStorageService',
         var appData = _getAppData();
 
         var data = { app: appData, profile: profileData, auth: authData }
-        
+
         $rootScope.user = data;
 
         return data;
