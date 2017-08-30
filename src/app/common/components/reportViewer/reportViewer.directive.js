204a60e7b7911c8241e62b123c0a20e88cc00223
Deepak Jain <deepjain1290@gmail.com>
2017-08-30 12:31:22 -0700

    change url in:build

--- src/app/common/components/reportViewer/reportViewer.directive.js
+++ src/app/common/components/reportViewer/reportViewer.directive.js
@@ -86,7 +86,7 @@
                 function _showResult() {
                     $("#progress-bar").show();
                     $("#print-button").hide();
-                    $scope.reportUrl = 'app/common/components/reportViewer/blank.html?rnd=1'
+                    $scope.reportUrl = 'app/common/components/reportViewer/blank.html'
                     console.log($scope.page.searchList)
                     if ($scope.page.pageinfo.filters.length > 0) {
                         if ($scope.page.searchList.length <= 0) {
--- src/app/common/services/authInterceptorService.js
+++ src/app/common/services/authInterceptorService.js
@@ -72,12 +72,13 @@ angular.module('BlurAdmin.common').factory('authInterceptorService', ['$q', '$lo
             || response.config.url.endsWith('GetFile')
             || response.config.url.endsWith('GetAttach')) {
         }
-        else if (response.config.url.indexOf('/api/') > 0) {
+        else if (response.config.url.indexOf('/api/') > -1) {
             var result = {};
             if (response.data !== undefined) {
                 var result_data = angular.fromJson(response.data);
                 result = result_data;
-
+                console.log(result_data.lz,'lz')
+                console.log(result_data,'result_data')
                 if (result_data.lz !== undefined) {
                     console.log('converting request')
                     if (result_data.lz) {
