540ac1a82d357714dd485544cebe50122c45ae33
Nitesh Mishra <nitesh.mishra@itsl.in>
2017-09-04 11:07:35 +0530

    nm

---
+++ src/app/pages/leave/reports/reports.module.js
@@ -0,0 +1,31 @@
+/**
+ * @author santosh.kushwaha
+ * created on 20/05/2017
+ */
+(function () {
+  'use strict';
+
+  angular.module('BlurAdmin.pages.leave.reports', [
+     'BlurAdmin.pages.leave.reports.monthlyLeaveDetail' ,
+     'BlurAdmin.pages.leave.reports.leaveDetail' 
+         
+       ])
+    .config(routeConfig);
+
+
+  /** @ngInject */
+  function routeConfig($stateProvider) {
+    $stateProvider
+      .state('leave.reports', {
+        url: '/reports',
+        template: '<ui-view  autoscroll="true" autoscroll-body-top></ui-view>',
+        abstract: true,
+        title: 'reports',
+        sidebarMeta: {
+          icon: 'ion-gear-a',
+          order: 100,
+        },
+      })
+  }
+
+})();
