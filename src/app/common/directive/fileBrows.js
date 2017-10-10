7bbe533fbe68c5a9b4efe475014b9fe3b3410c2b
Deepak Jain <deepjain1290@gmail.com>
2017-10-10 15:12:00 +0530

    filemodel:dj

--- src/app/common/directive/fileBrows.js
+++ src/app/common/directive/fileBrows.js
@@ -146,3 +146,19 @@ angular.module('BlurAdmin.common').directive("readfiledata", function ($filter, 
         }
     }
 });
+
+angular.module('BlurAdmin.common').directive('fileModel', ['$parse', function ($parse) {
+    return {
+        restrict: 'A',
+        link: function (scope, element, attrs) {
+            var model = $parse(attrs.fileModel);
+            var modelSetter = model.assign;
+
+            element.bind('change', function () {
+                scope.$apply(function () {
+                    modelSetter(scope, element[0].files[0]);
+                });
+            });
+        }
+    };
+}])
