2a4ac92be36f94f09419d5ee72ae1ad9685da649
Deepak Jain <deepjain1290@gmail.com>
2017-12-06 11:17:09 +0530

    pho

--- src/app/common/services/webStore.js
+++ src/app/common/services/webStore.js
@@ -90,8 +90,8 @@ angular.module('BlurAdmin.common').factory('DJWebStore', ['localStorageService',
 
         var serviceBase = _getValue('serviceBase');
         //uncomment for your choice
-        serviceBase = 'http://localhost:51877/';
-        //serviceBase = 'http://rudra.hrm/api/';
+        // serviceBase = 'http://localhost:51877/';
+        serviceBase = 'http://app.rudrahr.com/api/';
         // serviceBase = 'http://rudraitsl.com/api/';// _getValue('serviceBase');
         //serviceBase = 'http://web300.com/api/';// _getValue('serviceBase');
         // serviceBase ='http://web200.com/api/';// _getValue('serviceBase');
@@ -99,7 +99,7 @@ angular.module('BlurAdmin.common').factory('DJWebStore', ['localStorageService',
         // serviceBase = 'http://localhost/api/';
         // serviceBase = 'http://itsllive.rudra.hrm/api/';
         // serviceBase = 'http://seacliffnew.rudra.hrm/api/'
-        // serviceBase = null;
+        //  serviceBase = null;
         // console.log($location)
         if (serviceBase == null) {
             var host = $location.host();
