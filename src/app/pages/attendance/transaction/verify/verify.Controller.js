e2956b9b2244c7e10ca68e620d5f34de0fd67ae3
Deepak Jain <deepjain1290@gmail.com>
2017-12-20 14:34:39 +0530

    dj

--- src/app/pages/attendance/transaction/verify/verify.Controller.js
+++ src/app/pages/attendance/transaction/verify/verify.Controller.js
@@ -83,17 +83,17 @@
 
     $scope.page.boxOptions.columnDesign.push({
       name: 'TotalDeductableDays', visible: true, cellClass:
-      function (grid, row, col, rowRenderIndex, colRenderIndex) {
-        console.log(grid, row, col, rowRenderIndex, colRenderIndex)
-        var totDays = parseFloat(row.entity.TotalDeductableDays);
-        console.log(totDays)
-        if (totDays > 0) {
-          return 'RED-500 GRID-CELL-TEXT-RIGHT';
+        function (grid, row, col, rowRenderIndex, colRenderIndex) {
+          console.log(grid, row, col, rowRenderIndex, colRenderIndex)
+          var totDays = parseFloat(row.entity.TotalDeductableDays);
+          console.log(totDays)
+          if (totDays > 0) {
+            return 'RED-500 GRID-CELL-TEXT-RIGHT';
+          }
+          else {
+            return 'GREEN-300 GRID-CELL-TEXT-RIGHT'
+          }
         }
-        else {
-          return 'GREEN-300 GRID-CELL-TEXT-RIGHT'
-        }
-      }
     })
 
     $scope.page.boxOptions.columnDesign.push({ name: 'WeekOffPresent', visible: true, cellClass: 'GRID-CELL-TEXT-RIGHT' })
@@ -185,11 +185,72 @@
       }
     }
     function _downloadTemplate() {
+
+
+      // /* original data */
+      // var data = [[1, 2, 3], [true, false, null, "sheetjs"], ["foo", "bar", "0.3"], ["baz", null, "qux"]]
+      // var ws_name = "SheetJS";
+
+      // /* require XLSX */
+      // // var XLSX = require('xlsx')
+
+      // /* set up workbook objects -- some of these will not be required in the future */
+      // var wb = {}
+      // wb.Sheets = {};
+      // wb.Props = {};
+      // wb.SSF = {};
+      // wb.SheetNames = [];
+
+      // /* create worksheet: */
+      // var ws = {}
+
+      // /* the range object is used to keep track of the range of the sheet */
+      // var range = { s: { c: 0, r: 0 }, e: { c: 0, r: 0 } };
+
+      // /* Iterate through each element in the structure */
+      // for (var R = 0; R != data.length; ++R) {
+      //   if (range.e.r < R) range.e.r = R;
+      //   for (var C = 0; C != data[R].length; ++C) {
+      //     if (range.e.c < C) range.e.c = C;
+
+      //     /* create cell object: .v is the actual data */
+      //     var cell = { v: data[R][C] };
+      //     if (cell.v == null) continue;
+
+      //     /* create the correct cell reference */
+      //     var cell_ref = XLSX.utils.encode_cell({ c: C, r: R });
+
+      //     /* determine the cell type */
+      //     if (typeof cell.v === 'number') cell.t = 'n';
+      //     else if (typeof cell.v === 'boolean') cell.t = 'b';
+      //     else cell.t = 's';
+
+      //     /* add to structure */
+      //     ws[cell_ref] = cell;
+      //   }
+      // }
+      // ws['!ref'] = XLSX.utils.encode_range(range);
+
+      // /* add worksheet to workbook */
+      // wb.SheetNames.push(ws_name);
+      // wb.Sheets[ws_name] = ws;
+
+
+      // var wbout = XLSX.write(wb, { bookType: 'xlsx', type: 'binary' });
+      // var dataBinary = s2ab(wbout);
+      // // saveFileAs(dataBinary, contentType, filename)
+      // saveAs(new Blob([s2ab(wbout)], { type: "application/octet-stream" }), "sheetjs.xlsx");
+
+
+      // /* write file */
+      // XLSX.writeFile(wb, 'test.xlsx');
+
+
       console.log($scope.page)
       var tempColumns = [];
       if ($scope.page.gridOptions.data.length > 0) {
         angular.forEach($scope.page.gridOptions.data, function (data) {
-          
+
           var row = {
             'EmployeeCode': data.EmpCode,
             'EmployeeName': data.EmpName,
@@ -247,8 +308,10 @@
     }
     function _uploadRecord() {
 
-      pageService.getAttSummaryFile();
-      
+
+
+      //pageService.getAttSummaryFile();
+
       var options = {
         url: "app/common/forms/browseModal/browseModal.html",
         controller: "",
@@ -257,7 +320,14 @@
       dialogModal.open(options)
 
     }
-
+    /* generate a download */
+    function s2ab(s) {
+      var buf = new ArrayBuffer(s.length);
+      var view = new Uint8Array(buf);
+      for (var i = 0; i != s.length; ++i)
+        view[i] = s.charCodeAt(i) & 0xFF;
+      return buf;
+    }
     $scope.$on('uploadGridData', _upload)
     function _upload(evt, uploadGridData) {
 
