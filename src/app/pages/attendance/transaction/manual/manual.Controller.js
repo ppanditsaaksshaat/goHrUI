3bedb45dbec62454327d059a46720c3b6f2f918b
Santosh Kushwaha <tosantoshkushwaha@gmail.com>
2018-02-12 18:13:42 +0530

    santosh

--- src/app/pages/attendance/transaction/manual/manual.Controller.js
+++ src/app/pages/attendance/transaction/manual/manual.Controller.js
@@ -9,22 +9,28 @@
     .controller('attTransManualController', attTransManualController);
 
   /** @ngInject */
-  function attTransManualController($scope, $state, $timeout, pageService) {
+  function attTransManualController($scope, $state, $timeout, pageService, editFormService) {
 
     var vm = this;
     var currentState = $state.current;
 
 
+
+    $scope.showAdmin_Panel = false;
+    $scope.showadminremark = false;
     vm.filterOpt = {};
     vm.searchList = [];
     vm.orderByList = [];
     vm.queryId = 421;
-
+    vm.pageId = 320;
     this.applyFilter = _applyFilter;
     // this.uploadRecord = _uploadRecord;
-    $scope.entity = {}
+    $scope.entity = {};
+    vm.oldEntity = {};
     $scope.page = $scope.createPage();
     $scope.page.pageId = 320;
+    $scope.closeForm = _closeForm;
+
     $scope.page.boxOptions = {
       selfLoading: true,
       showRefresh: true,
@@ -41,8 +47,8 @@
       gridHeight: 450,
       getPageData: null,
       refreshData: null,
-      addRecord: null,
-      editRecord: null,
+      addRecord: _addRecord,
+      editRecord: _editRecord,
       updateRecord: null,
       viewRecord: null,
       deleteRecord: null,
@@ -51,6 +57,88 @@
       pageResult: _pageResult,
       defaultEntity: { 'AttDate': moment() }
       // readonlyColumns: ['col1', 'col2']
+    }
+
+
+    function _shiftDuration(entity) {
+      $scope.isResetShifAndLunch = true;
+      console.log(entity);
+      var shiftFrom = moment(entity.InTime, "HH:mm:ss")
+      var shiftTo = moment(entity.OutTime, "HH:mm:ss")
+      console.log(shiftFrom);
+      console.log(shiftTo);
+      // var duration = shiftTo.diff(shiftFrom, 'hours')
+      var duration = moment.duration(shiftTo.diff(shiftFrom))
+      console.log(duration)
+      var hours = parseInt(duration.asHours())
+
+      console.log(hours);
+      var minutes = parseInt(duration.asMinutes()) - hours * 60;
+      console.log(hours + ' hour and ' + minutes + ' minutes')
+      console.log(duration, hours)
+
+      var minute = shiftTo.diff(shiftFrom, 'minutes')
+      var timeDuration = minute / 60;
+      var durations = moment(duration, "HH:mm:ss a")
+      $scope.shiftDurations = hours + ' hour and ' + minutes + ' minutes';
+
+
+      // duration time for minimum hour for full day
+      $scope.durationTime = moment(hours + ':' + minutes, 'HH:mm:ss a');
+      console.log($scope.durationTime)
+      console.log($scope.durationTime.format("HH:mm"))
+      $scope.entity.SMMinimumHourForFullDay = $scope.durationTime.format("HH:mm");
+
+      //duration time for minimum hour for half day
+      var halfHour = hours / 2;
+      var halfMinute = minutes / 2;
+      $scope.halfDurationTime = moment(halfHour + ':' + halfMinute, 'HH:mm:ss a');
+      $scope.entity.SMMinimumHourForHalfDay = $scope.halfDurationTime.format("HH:mm");
+      console.log(halfHour, halfMinute)
+      console.log($scope.halfDurationTime, $scope.entity.SMMinimumHourForHalfDay)
+
+
+
+
+      // duration time for minimum hour for half day
+      // var halfMinutes = hours / 2;
+      // var halfHours = minutes / 2;
+      // var durationTimeForHalfDay = moment(halfMinutes + ':' + minutes, 'HH:mm:ss a');
+      // $scope.entity.SMMinimumHourForHalfDay = durationTimeForHalfDay.format("HH:mm");
+      // console.log(durationTimeForHalfDay,halfMinutes,halfHours)
+
+
+
+
+
+      console.log(duration, durations, minute, timeDuration)
+    }
+
+    function _editRecord(row) {
+
+
+      // if (($scope.user.isAdmin) || ($scope.user.isManager))
+      // {
+      //   $scope.showadminremark = true;
+      // }
+
+      console.log(row);
+      row.AttDate = moment(row.AttDate).format("DD-MM-YYYY");
+      console.log(moment(row.AttDate).format("DD-MM-YYYY"));
+      $scope.entity = row.entity;
+
+    //  $scope.entity = angular.copy(row.entity);
+      console.log( $scope.entity);
+     _shiftDuration($scope.entity);
+      console.log(row);
+      $scope.showEditForm = true;
+      // $scope.entity = row.entity;
+      $scope.showAdmin_Panel = true;
+    }
+
+    function _addRecord() {
+      $scope.showEditForm = true;
+      $scope.showAdmin_Panel = false;
     }
 
     function _loadController() {
@@ -79,25 +167,12 @@
         }
         console.log(filter.name)
 
-        // if (filter.name == 'Month') {
-        //   filter.value = parseInt(moment().format('MM'));
-        // }
-        // if (filter.name == 'Year') {
-        //   filter.value = parseInt(moment().format('YYYY'));
-        // }
-        // if (filter.name == 'VADepartmentId') {
-        //   filter.value = -1;
-        //   filter.disabled = true;
-        // }
       })
     }
 
-
-
     console.log($scope.page)
-
     $scope.clearFormCommon = _clearFormCommon;
-    $scope.closeForm = _closeForm;
+    // $scope.closeForm = _closeForm;
     $scope.saveForm = _saveForm;
 
     // var fieldEvents = [];
@@ -106,20 +181,43 @@
       console.log(obj + 'call')
     }
 
-
-
-
     function _uploadRecord() {
       $state.go("attendance.transaction.upload");
     }
     function _validateForm(editForm) {
       return true;
     }
-    function _saveForm(editForm) {
-      if (_validateForm(editForm)) {
-        console.log($scope.entity)
-      }
+    function _saveForm() {
+      alert('ss');
+      $scope.entity.AttToDate = $scope.entity.AttDate;
+      alert($scope.entity.AttId);
+      console.log($scope.entity);
+      editFormService.saveForm(vm.pageId, $scope.entity, vm.oldEntity,
+        $scope.entity.AttId === undefined ? "create" : "edit", $scope.page.pageinfo.title, $scope.editForm, true)
+        .then(_saveFormSuccessResult, _saveFormErrorResult);
     }
+
+    function _saveFormSuccessResult(result) {
+
+      alert('save');
+      console.log(result);
+      $scope.showEditForm = false;
+      $scope.entity = {};
+      $scope.page.refreshData();
+
+    }
+
+    function _saveFormErrorResult(error) {
+      console.log(error);
+
+    }
+
+
+
+
+
+
+
     function _resetFormCommon(editForm) {
 
     }
@@ -127,17 +225,14 @@
 
     }
     function _closeForm(editForm) {
+
       $scope.showEditForm = false;
       $scope.entity = {};
+
+
     }
-    function _addRecord() {
-      // $state.go("attendance.transaction.add", "{action:'create'}");
-      $scope.showEditForm = true;
-    }
-    function _editRecord(row) {
-      var empId = row.entity.EmpId;
-      $state.go("organization.employees.edit", { action: 'edit', empId: empId });
-    }
+
+
     function _updateRecord(row) {
       var empId = row.entity.EmpId;
       alert('_updateRecord called:' + empId)
