7a24cee3f05889b69f0628ae2c9e41eb95df0bea
Nitesh Kumar Mishra <nitesh.mishra@itsl.in>
2018-07-13 21:50:03 +0530

    NM

---
+++ src/app/pages/organization/reports/manPower/manPower.Controller.js
@@ -0,0 +1,19 @@
+/**
+ * @author v.lugovsky
+ * created on 16.12.2015
+ */
+(function () {
+  'use strict';
+
+  angular.module('BlurAdmin.pages.organization.reports.manPower')
+    .controller('manPowerReportController', manPowerReportController);
+
+  /** @ngInject */
+  function manPowerReportController($scope) {
+    $scope.page = { reportId: 74 }
+
+   
+
+  }
+
+})();
---
+++ src/app/pages/organization/reports/monthWiseManPower/monthWiseManPower.Controller.js
@@ -0,0 +1,19 @@
+/**
+ * @author v.lugovsky
+ * created on 16.12.2015
+ */
+(function () {
+  'use strict';
+
+  angular.module('BlurAdmin.pages.organization.reports.monthWiseManPower')
+    .controller('monthWiseManPowerReportController', monthWiseManPowerReportController);
+
+  /** @ngInject */
+  function monthWiseManPowerReportController($scope) {
+    $scope.page = { reportId: 75 }
+
+   
+
+  }
+
+})();
