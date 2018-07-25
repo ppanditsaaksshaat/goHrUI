a19d467c9efc6c44d129b0603bbd4cb425cafb58
niteshkumarmishra <nitesh.mishra@itsl.in>
2018-07-25 15:24:29 +0530

    NM:MSm

--- src/app/pages/self/attendance/miscellaneous/miscellaneous.controller.js
+++ src/app/pages/self/attendance/miscellaneous/miscellaneous.controller.js
@@ -17,6 +17,7 @@
         $scope.applyAttendance = _applyAttendance;
         $scope.applyOD = _applyOD;
         $scope.applyCOff = _applyCOff;
+        var isSearchingData = false;
 
         $scope.entity = {}
         $scope.page = $rootScope.createPage();
@@ -33,6 +34,7 @@
         $rootScope.$on("CallParentMethod", function () {
             $scope.employeeSalary = _employeeSalary();
         });
+
 
 
         $scope.page.boxOptions = {
@@ -59,30 +61,171 @@
             showDataOnLoad: true,
             isVerifyButton: true,
             columnDesign: [],
-            pageResult: _pageResult
+            viewRecord: _viewRecord
             // currentState: 'configuration.company.locations.location'
-        }
-
-        function _pageResult(result) {
-            console.log(result)
         }
 
         $scope.page.searchList.push({
             field: 'Month',
             operand: '>=',
-            value: 3
+            value: $scope.month
         })
         $scope.page.searchList.push({
             field: 'Year',
             operand: '<=',
-            value: '2018'
+            value: $scope.years
         })
         $scope.page.searchList.push({
             field: 'EmpId',
             operand: '=',
-            value: 5
+            value: $rootScope.user.profile.empId
             // value: $scope.user.profile.empId
         })
+
+        function _viewRecord(row) {
+            console.log('view')
+            console.log(row)
+            console.log(row.entity.DayStatus.substring(0, 7))
+            // $location.hash('bottom');
+            // // call $anchorScroll()
+            // $anchorScroll();
+            // console.log(monthSummery)
+            if (row.entity.DayStatus == 'Absent') {
+                $scope.apply = true;
+                $scope.applyCOf = false;
+            }
+
+            // var str = "Hello world!";
+            // var res = str.substring(1, 4);
+
+            else if (row.entity.DayStatus == 'Holiday' || row.entity.DayStatus == 'Weekoff' || row.entity.DayStatus.substring(0, 7) == 'Holiday') {
+                $scope.apply = false;
+                $scope.applyCOf = true;
+            }
+            else {
+                // $scope.showMsg("error", "Please Enter Date");
+                $scope.apply = false;
+                $scope.applyCOf = false;
+            }
+            $scope.applyDate = moment(row.entity.DATE).format('dd-MMM-yyyy');
+
+            $scope.holeObject = row.entity;
+            console.log('ng click work')
+        }
+
+        function _applyRequest(monthSummery) {
+            $location.hash('bottom');
+            // call $anchorScroll()
+            $anchorScroll();
+            console.log(monthSummery)
+            if (monthSummery.DayStatus == 'Absent') {
+                $scope.apply = true;
+                $scope.applyCOf = false;
+            }
+            else {
+                $scope.apply = false;
+                $scope.applyCOf = true;
+            }
+            $scope.applyDate = moment(monthSummery.DATE).format('dd-MMM-yyyy');
+
+            $scope.holeObject = monthSummery;
+            console.log('ng click work')
+        }
+
+        function _employeeSalary() {
+            var currentMonth = (moment().month()) + 1;
+            var currentYear = moment().year();
+            if ($scope.years <= currentYear) {
+                if ($scope.month <= currentMonth) {
+                    if (isSearchingData) {
+                        _searchAttendance()
+                    }
+                    isSearchingData = true;
+
+                    var searchLists = [];
+                    searchLists.push({
+                        field: 'Month',
+                        operand: "=",
+                        value: $scope.month
+                    })
+                    searchLists.push({
+                        field: 'Year',
+                        operand: "=",
+                        value: $scope.years
+                    })
+
+                    searchLists.push({
+                        field: 'EmpId',
+                        operand: "=",
+                        value: $rootScope.user.profile.empId
+                        // value: $scope.user.profile.empId
+                    })
+                    console.log(searchLists)
+                    var data = {
+                        searchList: searchLists,
+                        orderByList: []
+                    }
+                    pageService.getCustomQuery(data, 646).then(_getCustomQuerySuccessResult, _getCustomQueryErrorResult)
+                }
+                else {
+                    // $scope.showMsg("info", "Month and year should be less than or equal to current month and year");
+                }
+            }
+            else {
+                // $scope.showMsg("info", "Year should be less than or equal to current year");
+            }
+        }
+
+        function _getCustomQuerySuccessResult(result) {
+            $scope.monthlySummery.summery = [];
+
+            console.log(result)
+            console.log(result[0])
+            console.log(result[1])
+            $scope.miscelAttendanceSummery.monthlyAttDetails = result[1];
+            $scope.monthlySummery.summery = result[2];
+            $scope.empCode = result[0][0].EmpCode;
+            $scope.empName = result[0][0].EmpName;
+            $scope.monthlySummery.yearList = result[3]
+            $scope.monthlySummery.monthList = result[4]
+        }
+
+        function _getCustomQueryErrorResult(error) {
+            console.log(error);
+        }
+
+        function _searchAttendance() {
+            $scope.page.searchList = [];
+            $scope.page.searchList.push({
+                field: 'Month',
+                operand: '>=',
+                value: $scope.month
+            })
+            $scope.page.searchList.push({
+                field: 'Year',
+                operand: '<=',
+                value: $scope.years
+            })
+            $scope.page.searchList.push({
+                field: 'EmpId',
+                operand: '=',
+                value: $rootScope.user.profile.empId
+                // value: $scope.user.profile.empId
+            })
+            $scope.page.refreshData()
+        }
+
+
+
+
+
+
+        function _loadController() {
+        }
+
+
+
+
 
 
         function _goApplyReguest() {
@@ -116,84 +259,6 @@
             })
         }
 
-
-        function _applyRequest(monthSummery) {
-            $location.hash('bottom');
-            // call $anchorScroll()
-            $anchorScroll();
-            console.log(monthSummery)
-            if (monthSummery.DayStatus == 'Absent') {
-                $scope.apply = true;
-                $scope.applyCOf = false;
-            }
-            else {
-                $scope.apply = false;
-                $scope.applyCOf = true;
-            }
-            $scope.applyDate = moment(monthSummery.DATE).format('dd-MMM-yyyy');
-
-            $scope.holeObject = monthSummery;
-            console.log('ng click work')
-        }
-        function _loadController() {
-        }
-
-
-        function _employeeSalary() {
-            // var d = moment();
-            // var month = d.month();
-            // var year = d.year();
-            // $scope.month = month + 1;
-            // $scope.years = d.year();
-            // console.log(month, year)
-
-            var searchLists = [];
-            searchLists.push({
-                field: 'Month',
-                operand: "=",
-                value: $scope.month
-            })
-            searchLists.push({
-                field: 'Year',
-                operand: "=",
-                value: $scope.years
-            })
-            // searchLists.push({
-            //     field: 'SubUnitId',
-            //     operand: "=",
-            //     value: '2'
-            // })
-            searchLists.push({
-                field: 'EmpId',
-                operand: "=",
-                value: $rootScope.user.profile.empId
-                // value: $scope.user.profile.empId
-            })
-            console.log(searchLists)
-            var data = {
-                searchList: searchLists,
-                orderByList: []
-            }
-            pageService.getCustomQuery(data, 646).then(_getCustomQuerySuccessResult, _getCustomQueryErrorResult)
-        }
-
-        function _getCustomQuerySuccessResult(result) {
-            $scope.monthlySummery.summery = [];
-
-            console.log(result)
-            console.log(result[0])
-            console.log(result[1])
-            $scope.miscelAttendanceSummery.monthlyAttDetails = result[1];
-            $scope.monthlySummery.summery = result[2];
-            $scope.empCode = result[0][0].EmpCode;
-            $scope.empName = result[0][0].EmpName;
-            $scope.monthlySummery.yearList = result[3]
-            $scope.monthlySummery.monthList = result[4]
-        }
-
-        function _getCustomQueryErrorResult(error) {
-            console.log(error);
-        }
 
         _employeeSalary();
     }
