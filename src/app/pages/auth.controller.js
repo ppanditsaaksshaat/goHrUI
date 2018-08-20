1d961a8459042be4bd112ece2c2c4868c337a3cf
Deepak Jain <deepjain1290@gmail.com>
2018-08-20 10:35:01 +0530

    dj

--- src/app/pages/auth.controller.js
+++ src/app/pages/auth.controller.js
@@ -178,7 +178,7 @@
                         if (result.key) {
                             $scope.key = result.key;
                             isValid = result.key.vl
-                        }
+                        } 
 
                         if (isValid) {
                             var profileData = result;
